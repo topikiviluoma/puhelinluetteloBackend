b921856a78654be035474ff26b52eb7e7bb98883
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2018-08-09 16:11:15 +0300

    t3.1-3.6

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-const http = require('http')
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
+
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/people', (req, res) => {
+app.get('/info', (req, res) => {
+    const info = `<p>puhelinluettelossa on ${people.length} henkilön tiedot </p> 
+    ${new Date().toJSON()}`
+    
+    res.send(info)
+})
+
+app.get('/api/people', (req, res) => {
     res.json(people)
 })
 
-app.get('/people/:id', (req, res) => {
+app.get('/api/people/:id', (req, res) => {
     const id = Number(req.params.id)
     const person = people.find(person => person.id === id)
     if (person) {
@@ -20,6 +29,44 @@ app.get('/people/:id', (req, res) => {
         res.status(404).end()
     }
 })
+
+app.post('/api/people', (req, res) => {
+    const body = req.body
+    if (body.name === undefined) {
+        return res.status(400).json({error: 'name is missing'})
+    }
+
+    if (body.number === undefined) {
+        return res.status(400).json({error: 'number is missing'})
+    }
+
+    if (people.find(person => person.name === body.name) !== undefined) {
+        return res.status(400).json({error: 'name must be unique'})
+    }
+    
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+    console.log(person)
+    people = people.concat(person)
+    res.json(person)
+})
+
+app.delete('/api/people/:id', (req, res) => {
+    const id = Number(req.params.id)
+    people = people.filter(person => person.id != id)
+    res.status(204).end()
+})
+
+
+const generateId = () => {
+    return Math.floor((Math.random() * 999) + 1)
+}
+
+const generateNumber = () => {
+}
 
 
 const port = 3001
@@ -46,5 +93,10 @@ let people = [
         "name": "Lea Kutvonen",
         "number": "040-123456",
         "id": 4
+    },
+    {
+        "name": "Test",
+        "number": "040-123456",
+        "id": 5
     }
 ]
