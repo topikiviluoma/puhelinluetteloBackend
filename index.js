85c8d64bba4baec11cdca6f9f8e95479bb1d0fd9
Topi Kiviluoma <topi.kiviluoma@helsinki.fi>
2018-08-12 15:14:20 +0300

    Heruko commit

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const generateNumber = () => {
 }
 
 
-const PORT = process.env.PORT || 3001
+const port = process.env.PORT || 3001
 app.listen(port)
 console.log(`Server running on port ${port}`)
 
